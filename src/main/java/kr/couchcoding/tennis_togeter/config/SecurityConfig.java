c5c322031b57654b78cfea8ee11a54f4d1e729fd
godongs <asd236678@gmail.com>
2021-10-27 13:28:29 +0900

    Security 설정 추가

---
+++ src/main/java/kr/couchcoding/tennis_togeter/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package kr.couchcoding.tennis_togeter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+  
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // 인증 예외 URL설정
+        web.ignoring().antMatchers(HttpMethod.POST, "/users")
+            .antMatchers("/")
+            .antMatchers("/resources/**");
+    } 
+}
