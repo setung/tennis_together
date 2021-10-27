6b05187f9c23c63a0effb545b758344f4bd2db79
godongs <asd236678@gmail.com>
2021-10-27 13:17:52 +0900

    init spring

---
+++ src/main/java/kr/couchcoding/tennis_togeter/TennisTogeterApplication.java
@@ -0,0 +1,13 @@
+package kr.couchcoding.tennis_togeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisTogeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisTogeterApplication.class, args);
+	}
+
+}
