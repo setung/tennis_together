c95d62401f09b9e34ca62116aeade93f625ecedb
DESKTOP-BTTJJGD\4evaj <4evajsh@gmail.com>
2021-11-08 17:38:27 +0900

    add game get API

--- src/main/java/kr/couchcoding/tennis_together/domain/game/dao/GameRepository.java
+++ src/main/java/kr/couchcoding/tennis_together/domain/game/dao/GameRepository.java
@@ -2,6 +2,7 @@ package kr.couchcoding.tennis_together.domain.game.dao;
 
 import kr.couchcoding.tennis_together.domain.game.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface GameRepository extends JpaRepository<Game, Long> {
+public interface GameRepository extends JpaRepository<Game, Long>, JpaSpecificationExecutor<Game> {
 }
