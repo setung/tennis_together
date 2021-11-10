5d52efc92414595630372a83e4ff16c65c03a91c
DESKTOP-BTTJJGD\4evaj <4evajsh@gmail.com>
2021-11-11 03:33:18 +0900

    add GameUserList Apply api

--- src/main/java/kr/couchcoding/tennis_together/domain/game/model/GameUserList.java
+++ src/main/java/kr/couchcoding/tennis_together/domain/game/model/GameUserList.java
@@ -7,10 +7,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
 @NoArgsConstructor
---
+++ src/main/java/kr/couchcoding/tennis_together/domain/game/service/GameUserListService.java
@@ -0,0 +1,42 @@
+package kr.couchcoding.tennis_together.domain.game.service;
+
+import kr.couchcoding.tennis_together.domain.game.dao.GameUserListRepository;
+import kr.couchcoding.tennis_together.domain.game.model.Game;
+import kr.couchcoding.tennis_together.domain.game.model.GameUserList;
+import kr.couchcoding.tennis_together.domain.game.status.GameUserListStatus;
+import kr.couchcoding.tennis_together.domain.user.model.User;
+import kr.couchcoding.tennis_together.exception.CustomException;
+import kr.couchcoding.tennis_together.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class GameUserListService {
+
+    private final GameService gameService;
+    private final GameUserListRepository gameUserListRepository;
+
+    public void applyGame(User user, Long gameNo) {
+        Game game = gameService.findGameByNo(gameNo);
+
+        if (game.getGameCreator().getUid().equals(user.getUid()))
+            throw new CustomException(ErrorCode.BAD_REQUEST_GAME, "자신의 게임에 신청할 수 없습니다.");
+
+        gameUserListRepository.findByGameUserAndJoinedGame(user, game)
+                .ifPresent(gameUserList -> {
+                    throw new CustomException(ErrorCode.BAD_REQUEST_GAME, "이미 신청한 게임입니다.");
+                });
+
+        GameUserList newGameUserList = GameUserList.builder()
+                .joinedGame(game)
+                .gameUser(user)
+                .stDvCd(GameUserListStatus.APPLIED)
+                .build();
+
+        gameUserListRepository.save(newGameUserList);
+    }
+}
