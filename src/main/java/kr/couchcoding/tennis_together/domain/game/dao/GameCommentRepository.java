8fd853fe890102188050773f668fbcfe0754c240
jiinahn92@gmail.com <jiinahn92@gmail.com>
2021-11-16 21:03:16 +0900

    댓글삭제 API 추가

--- src/main/java/kr/couchcoding/tennis_together/domain/game/dao/GameCommentRepository.java
+++ src/main/java/kr/couchcoding/tennis_together/domain/game/dao/GameCommentRepository.java
@@ -3,14 +3,21 @@ package kr.couchcoding.tennis_together.domain.game.dao;
 
 import kr.couchcoding.tennis_together.domain.game.model.Game;
 import kr.couchcoding.tennis_together.domain.game.model.GameComment;
+import kr.couchcoding.tennis_together.domain.user.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface GameCommentRepository extends JpaRepository<GameComment,Long>  { // <Entity,id type(pk)>
 
+
     Page<GameComment> findByCommentedGame(Game commentedGame, Pageable pageable); // Entity에서 gameNo = CommentedGame
+
+    Optional<GameComment> findByCommentNo(Long commentNo);
 
 }
