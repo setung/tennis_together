a84c18d8151f1a2d6712639c1282def3dd7fb582
DESKTOP-BTTJJGD\4evaj <4evajsh@gmail.com>
2021-11-17 16:47:48 +0900

    모든 Repository에 @Repository 추가

--- src/main/java/kr/couchcoding/tennis_together/domain/location/dao/LocCdRepository.java
+++ src/main/java/kr/couchcoding/tennis_together/domain/location/dao/LocCdRepository.java
@@ -3,9 +3,11 @@ package kr.couchcoding.tennis_together.domain.location.dao;
 import kr.couchcoding.tennis_together.domain.location.model.LocCd;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface LocCdRepository extends JpaRepository<LocCd, Long>, JpaSpecificationExecutor<LocCd> {
     Optional<LocCd> findByLocSdAndLocSkk(String locSd, String locSkk);
 }
