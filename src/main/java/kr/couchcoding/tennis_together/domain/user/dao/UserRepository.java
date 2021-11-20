da93196f3c7cf5391ee0ae4c858944bab5f7ecaa
ilhoon93 <kohkhj902@gmail.com>
2021-11-20 16:57:27 +0900

    feature check nickname

--- src/main/java/kr/couchcoding/tennis_together/domain/user/dao/UserRepository.java
+++ src/main/java/kr/couchcoding/tennis_together/domain/user/dao/UserRepository.java
@@ -8,9 +8,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import kr.couchcoding.tennis_together.domain.user.model.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String>{
 
     Page<User> findByLocCd(LocCd locCd, Pageable pageable);
+
+    Optional<User> findByNickname(String nickname);
 
 }
