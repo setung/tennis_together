20783f6dccb0d78241f3aa0c8c88ca52a7ea9920
setung <4evajsh@gmail.com>
2021-12-11 05:09:52 +0900

    FirebaseConfig 수정

--- src/main/java/kr/couchcoding/tennis_together/config/FirebaseConfig.java
+++ src/main/java/kr/couchcoding/tennis_together/config/FirebaseConfig.java
@@ -6,43 +6,61 @@ import com.google.cloud.storage.StorageOptions;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import net.minidev.json.JSONObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class FirebaseConfig {
 
-    ClassPathResource resource = new ClassPathResource("config/firebaseKey.json");
-
     @Bean
-    public FirebaseAuth firebaseAuth() {
-        FirebaseOptions options = null;
-        try {
-            options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
-                    .build();
+    public FirebaseAuth firebaseAuth() throws IOException {
 
-            FirebaseApp.initializeApp(options);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream((getFirebaseIs())))
+                .build();
+
+        FirebaseApp.initializeApp(options);
         return FirebaseAuth.getInstance(FirebaseApp.getInstance());
     }
 
     @Bean
-    public Storage storage() {
-        Storage storage = null;
-        try {
-            storage = StorageOptions.newBuilder().
-                    setCredentials(GoogleCredentials.fromStream(resource.getInputStream())).
-                    setProjectId("tennis-together-91e65.appspot.com").build().getService();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+    public Storage storage() throws IOException {
+        Storage storage = StorageOptions.newBuilder().
+                setCredentials(GoogleCredentials.fromStream(getFirebaseIs())).
+                setProjectId("tennis-together-91e65.appspot.com").build().getService();
 
         return storage;
+    }
+
+    private InputStream getFirebaseIs() throws IOException {
+        ClassPathResource resource = new ClassPathResource("config/firebaseKey.json");
+        if (resource.exists())
+            return resource.getInputStream();
+
+        JSONObject jsonObject = new JSONObject(getEnvMap());
+        return new ByteArrayInputStream(jsonObject.toString().getBytes());
+    }
+
+    private Map getEnvMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("type", System.getenv().get("type"));
+        map.put("project_id", System.getenv().get("project_id"));
+        map.put("private_key_id", System.getenv().get("private_key_id"));
+        map.put("private_key", System.getenv().get("private_key").replace("\\n", "\n"));
+        map.put("client_email", System.getenv().get("client_email"));
+        map.put("client_id", System.getenv().get("client_id"));
+        map.put("auth_uri", System.getenv().get("auth_uri"));
+        map.put("token_uri", System.getenv().get("token_uri"));
+        map.put("auth_provider_x509_cert_url", System.getenv().get("auth_provider_x509_cert_url"));
+        map.put("client_x509_cert_url", System.getenv().get("client_x509_cert_url"));
+        return map;
     }
 }
